33540019e6d2b4905ba13f8122d4e2dae5a1ce73
bhalder003 <basudhahalder003@gmail.com>
2021-07-23 01:31:50 +0530

    listbox working

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Index msg="I should look " :options=options :selected=selected />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Index from './components/Index.vue'
+import vSelect from 'vue-select'
+Vue.component('v-select', vSelect)
+import 'vue-select/dist/vue-select.css'
+
+export default {
+  name: 'App',
+  components: {
+     Index
+  },
+  data () {
+    return {
+      options:['black', 'red', 'blue', 'green', 'purple', 'orange'],
+      selected : ''
+    }
+  },
+  created() {
+    setTimeout(() => {
+      this.selected = 'black';
+    }, 100);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="div-row">
+      
+      <h1 class = "select-dropdown">Color</h1>
+
+      <v-select 
+        class = "select-dropdown" 
+        v-model="selected" 
+        :options="options"
+        placeholder="select a color">
+      </v-select>
+      
+      <h1 :style="{color: selected}">{{ msg }} {{selected}}</h1>
+    </div>  
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  props: {
+    msg: String,
+    options: Array,
+    selected: String
+  }
+}
+</script>
+
+<style scoped>
+.div-row {
+ width: 1000px 
+}
+.select-dropdown {
+  width: 250px;
+  display: inline-block; 
+}
+
+h1 {
+  width: 450px;
+  display: inline-block; 
+}
+</style>
